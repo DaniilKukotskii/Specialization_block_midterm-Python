e58d233e2541b29517a386de6b526a3d461a7a09
DaniilKukotskii <daniilkukotsky@gmail.com>
2024-01-23 18:11:23 +0400

    The basic version of the application has been written. Changes are planned to improve convenience.

--- view/menu_view.py
+++ view/menu_view.py
@@ -4,9 +4,37 @@ class MenuView:
         print("2. Read Notes")
         print("3. Edit Note")
         print("4. Delete Note")
-        print("5. Save to JSON")
-        print("6. Save to CSV")
-        print("7. Exit")
+        print("5. Exit")
 
     def get_user_choice(self):
         return input("Enter your choice: ")
+
+    def display_note_titles(self, note_titles):
+        if note_titles:
+            print("Available Note Titles:")
+            for title in note_titles:
+                print(f"- {title}")
+        else:
+            print("No notes available.")
+
+    def create_note(self):
+        title = input("Enter the title of the note: ")
+        body = input("Enter the body of the note: ")
+        return title, body
+
+    def read_notes(self, note_titles):
+        self.display_note_titles(note_titles)
+        choice = input("Enter the title of the note to read: ")
+        return choice
+
+    def edit_note(self, note_titles):
+        self.display_note_titles(note_titles)
+        choice = input("Enter the title of the note to edit: ")
+        new_title = input("Enter the new title: ")
+        new_body = input("Enter the new body: ")
+        return choice, new_title, new_body
+
+    def delete_note(self, note_titles):
+        self.display_note_titles(note_titles)
+        choice = input("Enter the title of the note to delete: ")
+        return choice
--- view/note_view.py
+++ view/note_view.py
@@ -1,10 +1,6 @@
 class NoteView:
-    def display_notes(self, notes):
-        for note in notes:
-            print(f"ID: {note.note_id}\nTitle: {note.title}\nBody: {note.body}\nTimestamp: {note.timestamp}\n{'='*30}")
-
-    def display_message(self, message):
-        print(message)
-
-    def get_user_input(self, prompt):
-        return input(prompt)
+    def display_note(self, note):
+        print(f"Note ID: {note.note_id}")
+        print(f"Title: {note.title}")
+        print(f"Body: {note.body}")
+        print(f"Timestamp: {note.timestamp}")
