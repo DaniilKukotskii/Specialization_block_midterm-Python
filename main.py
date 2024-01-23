d1939a8da912b7299c99c461015e2f5950e3427b
DaniilKukotskii <daniilkukotsky@gmail.com>
2024-01-23 18:12:01 +0400

    The basic version of the application has been written. Changes are planned to improve convenience.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from model.note_manager import NoteManager
-from view.note_view import NoteView
+from presenter.note_presenter import NotePresenter
 from view.menu_view import MenuView
-from presenter.presenter import NotePresenter
+from view.note_view import NoteView
+from model.note_manager import NoteManager
+
 
 if __name__ == "__main__":
-    note_manager = NoteManager()
+    file_path = "notes.csv"  # путь к файлу CSV
+    note_manager = NoteManager(file_path)
     note_view = NoteView()
     menu_view = MenuView()
     presenter = NotePresenter(note_view, menu_view, note_manager)
