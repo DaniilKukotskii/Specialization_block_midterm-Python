affa88eb44d6999ace12d6e2114711ec1e7c8f7e
DaniilKukotskii <daniilkukotsky@gmail.com>
2024-01-26 16:14:14 +0400

    Filtering has been added when reading notes, minor changes have been made.

--- view/menu_view.py
+++ view/menu_view.py
@@ -9,13 +9,15 @@ class MenuView:
     def get_user_choice(self):
         return input("Enter your choice: ")
 
-    def read_notes(self, notes):
+    def read_notes(self, notes, filtered=False):
         if notes:
             print("Available Notes:")
             for note in notes:
                 print(f"- {note.title} (ID: {note.note_id})")
-        else:
-            print("No notes available.")
+            if filtered:
+                print("Notes are filtered.")
+            else:
+                print("No notes available.")
 
     def create_note(self):
         title = input("Enter the title of the note (press Enter to return to the main menu): ")
@@ -24,10 +26,10 @@ class MenuView:
         body = input("Enter the body of the note (press Enter to return to the main menu): ")
         return title, body
 
-    def edit_note(self):
-        new_title = input("Enter the new title: ")
-        new_body = input("Enter the new body: ")
-        return new_title, new_body
+    def edit_note(self, notes):
+        self.read_notes(notes)
+        note_id = input("Enter the ID of the note to edit (press Enter to return to the main menu): ")
+        return note_id if note_id.strip() else None
 
     def delete_note(self, notes):
         self.read_notes(notes)
@@ -37,3 +39,15 @@ class MenuView:
     def get_note_id(self):
         note_id = input("Enter the ID of the note (press Enter to return to the main menu): ")
         return note_id if note_id.strip() else None
+
+    def get_filter_choice(self, action):
+        print(f"{action} Filter Options:")
+        print("1. Sort by date (from new to old)")
+        print("2. Sort by date (from old to new)")
+        return input("Enter your choice (press Enter to skip): ")
+
+    def get_edit_choice(self):
+        print("Edit Options:")
+        print("1. Edit Title")
+        print("2. Edit Body")
+        return input("Enter your choice (press Enter to skip): ")
